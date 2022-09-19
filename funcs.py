7840a5e88c9c56b4a383b8292a743b9250e26954
chethanreddy1 <95535293+chethanreddy1@users.noreply.github.com>
2022-09-19 13:48:32 +0530

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -12,6 +12,7 @@ def playsound(length):
     #play a sound for 'length' seconds
     return None
 
+
 def doll(facing):
 
     #return 1080x384 bgr image array of doll facing front if input facing is a string 'front' and back if 
@@ -20,17 +21,30 @@ def doll(facing):
     img=np.zeros((1080,384,3))
     return img
 
+
 def pose(image):
 
-    #return array of size (n,34) where n is number of humans and second axis is [y_0, x_0, y_1, x_1, …, y_16, x_16]
-    #where y_i, x_i are the yx-coordinates of the i-th body part correspondingly
+    #return array of size (n,17,2) where n is number of humans and second and third axis is [[y_0, x_0], [y_1, x_1], …, [y_16, x_16]]
+    #where y_i, x_i are the yx-coordinates of the i-th body part of nth human correspondingly
 
-    ps=np.zeros((4,34))
-    return ps
+    im=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    im = tf.expand_dims(im, axis=0)
+    im = tf.cast(tf.image.resize(im, (288, 512)), dtype=tf.int32)
+    outputs = movenet(im)
+    keypoints = outputs['output_0'].numpy()[:,:,:51].reshape((6,17,3))
+    coords=keypoints[:,:,:2]
+    conf=keypoints[:,:,2]
+    l=[]
+    for i in range(len(conf)):
+        if np.sum(conf[i])<1.7:
+            l.append(i)
+    coords=np.delete(coords,l,axis=0)
+    return np.uint8(np.squeeze(np.multiply(coords, [image.shape[0],image.shape[1]])))
+
 
 def sortx(pose):
 
-    #input is 'pose' returned in function pose() return (n,34) array where 0th axis is sorted based on 
+    #input is 'pose' returned in function pose() return (n,17,2) array where 0th axis is sorted based on 
     # average x axis value of corresponding body part coordinates
     return pose
 
@@ -42,16 +56,22 @@ def error(pfpose,fpose):
     er=0
     return er
 
+
 def playerbar(playerstatus):
     #return 216x1536 image showing status of players visually
 
-    bar=np.zeros((216,1536))
+    bar=np.zeros((216,1536,3))
     return bar
 
-def hlightstatus(playerstatus,frame,fpose):
-    #highlight players red if player status is dead and green if alive
-    #using coordinates from fpose on frame
 
-    return frame
+def hlightstatus(playerstatus,frame,fpose):
+    image=frame.copy()
+    #highlight players red if player status is dead and green if alive
+    #using coordinates from fpose on frame. resize final image to 864x1536
+
+    return cv2.resize(image,(1536,864))
+
+
+
 
 
